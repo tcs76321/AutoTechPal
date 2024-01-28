0d41986989b13661c988704dd982648265d664e7
Trevor Stahl <trevor@trevorstahl.com>
2024-01-27 20:54:44 -0600

    add buttons and position properly

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { appStyles } from "./src/styles/AppStyles"; // Import styles
 
 export default function App() {
@@ -8,6 +7,33 @@ export default function App() {
         <View style={appStyles.container}>
             <View style={appStyles.titleContainer}>
                 <Text style={appStyles.titleText}>AutoTechPal</Text>
+            </View>
+            <View style={appStyles.bodyContainer}>
+                <View style={appStyles.row}>
+                    <Button
+                        title="VIN Lookup"
+                        onPress={() => {}}
+                    />
+                    <Button
+                        title="Document Repair"
+                        onPress={() => {}}
+                    />
+                </View>
+                <View style={appStyles.row}>
+                    <Button title="DTC Lookup" onPress={() => {}} />
+                    <Button title="Maint Schedule" onPress={() => {}} />
+                </View>
+                <View style={appStyles.row}>
+                    <Button title="BG Products" onPress={() => {}} />
+                    <Button title="Cost Estimator" onPress={() => {}} />
+                </View>
+                <View style={appStyles.row}>
+                    <Button title="Parts Finder" onPress={() => {}} />
+                    {/*<Button title="Button 8" onPress={() => {}} />*/}
+                </View>
+            </View>
+            <View style={appStyles.footerContainer}>
+                <Text style={appStyles.footerText}>Made by Trevor C. Stahl</Text>
             </View>
         </View>
     );
--- src/styles/AppStyles.ts
+++ src/styles/AppStyles.ts
@@ -10,7 +10,7 @@ export const appStyles = StyleSheet.create({
   titleContainer: {
     alignSelf: 'flex-start',
     backgroundColor: colors.darkBackground,
-    paddingTop: 10,
+    paddingTop: 20,
     paddingLeft: 10,
   },
   titleText: {
@@ -18,7 +18,35 @@ export const appStyles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
   },
+  bodyContainer: {
+    backgroundColor: colors.darkBackground,
+    flex: 1,
+    padding: 10,
+    justifyContent: "flex-start",
+  },
+  bodyText: {
+    color: colors.text_color,
+    fontSize: 16,
+  },
   mainText: {
     color: colors.text_color,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10,
+    paddingBottom: 30,
+    paddingTop: 30,
+  },
+  footerContainer: {
+    backgroundColor: colors.darkBackground,
+    padding: 10,
+    justifyContent: "center",
+    paddingBottom: 20,
+  },
+  footerText: {
+    color: colors.text_color,
+    fontSize: 12,
+    textAlign: "center",
   },
 });
